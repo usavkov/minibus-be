4b4f9a59305c950e93a7f6b42acdf371dff58e8d
Yury Saukou <yury_saukou@epam.com>
2022-10-23 20:28:01 +0400

    Update code style issues

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
+
+const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT, () => {
+    console.info(`Server started on port ${PORT}`);
+  });
 }
+
 bootstrap();
