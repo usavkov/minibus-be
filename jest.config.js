03aadd86e5f1e783437f246f2dbe566ecf0ef985
Yury Saukou <yury_saukou@epam.com>
2022-10-24 15:42:44 +0400

    Update tests config; define coverage dir

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.(spec|test)\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../artifacts/coverage-jest/',
+  testEnvironment: 'node',
+};
