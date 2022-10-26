75fbdfec531e247cc5beeed2f2b6cd1ca33a000a
Yury Saukou <yury_saukou@epam.com>
2022-10-26 21:22:23 +0400

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: 'src',
+  roots: ['<rootDir>/src/', '<rootDir>/test/'],
   testRegex: '.*\\.(spec|test)\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
